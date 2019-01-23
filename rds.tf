4c5ec4537445be26d49bd9f928335e70dbc0dc95
Neil McBride <Neil.McBride@digital.homeoffice.gov.uk>
2019-01-23 11:07:28 +0000

    Put rds creds into param store

--- rds.tf
+++ rds.tf
@@ -111,3 +111,15 @@ resource "aws_db_instance" "postgres" {
     Name = "ext_tableau-postgres-${local.naming_suffix}"
   }
 }
+
+resource "aws_ssm_parameter" "rds_external_tableau_username" {
+  name  = "rds_external_table_username"
+  type  = "SecureString"
+  value = "${random_string.username.result}"
+}
+
+resource "aws_ssm_parameter" "rds_external_tableau_password" {
+  name  = "rds_external_tableau_password"
+  type  = "SecureString"
+  value = "${random_string.password.result}"
+}
